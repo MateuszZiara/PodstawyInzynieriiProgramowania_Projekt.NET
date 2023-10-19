ed0a89052919a01e4d762a8630f375a5e3482cff
Student092767 <s092767@student.kielce.pl>
2023-10-19 15:41:05 +0200

    Poprawiony KlientEntity ;)

--- Projekt Sklep/Models/Klient/KlientEntity.cs
+++ Projekt Sklep/Models/Klient/KlientEntity.cs
@@ -7,33 +7,28 @@ namespace Projekt_Sklep.Models.Klient
     {
         public KlientEntity() : base() 
         { }
-        public KlientEntity(Guid id, string Name, string LastName, string NIP, string Regon, string Nazwa_Firmy, string Pan_Pani)
-        { 
-            this.Id = id;
-            this.Imie = Name;
-            this.Nazwisko = LastName;
+        public KlientEntity(Guid Id, string Name, string Pesel, string NumerTelefonu, string Email, string NIP, string LastName, Guid AdresID, Guid PolisaID)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.LastName = LastName;
+            this.Pesel = Pesel;
+            this.NumerTelefonu = NumerTelefonu;
+            this.Email = Email;
             this.NIP = NIP;
-            this.REGON = Regon;
-            this.Nazwa_firmy = Nazwa_Firmy;
-            this.Pan_Pani = Pan_Pani;
-            
+            this.AdresID = AdresID;
+            this.PolisaID = PolisaID;
+
         }
-        [Required(ErrorMessage = "Pole \"Id\" jest wymagane.")]
         public virtual Guid Id { get; set; }
-        [Required(ErrorMessage = "Pole \"Name\" jest wymagane.")]
-        public virtual string Imie { get; set; }
-        [Required(ErrorMessage = "Pole \"LastName\" jest wymagane.")]
-        public virtual string Nazwisko { get; set; }
-
-        [Required(ErrorMessage = "Pole \"NIP\" jest wymagane.")]
+        public virtual string Name { get; set; }
+        public virtual string LastName { get; set; }
+        public virtual string Pesel { get; set; }
+        public virtual string NumerTelefonu { get; set; }
+        public virtual string Email { get; set; }
         public virtual string NIP { get; set; }
+        public virtual Guid AdresID  { get; set; }
+        public virtual Guid PolisaID { get; set; }
 
-        [Required(ErrorMessage = "Pole \"REGON\" jest wymagane.")]
-        public virtual string REGON { get; set; }
-
-        [Required(ErrorMessage = "Pole \"Nazwa_firmy\" jest wymagane.")]
-        public virtual string Nazwa_firmy { get; set;}
-        [Required(ErrorMessage = "Pole \"Pan_Pani\" jest wymagane.")]
-        public virtual string Pan_Pani { get; set; }
     }
 }
