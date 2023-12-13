19d77e97ab0bb87716987fa4939e5fdf396996cb
Mateusz Ziara <mateusz.ziara@gmail.com>
2023-12-13 20:02:20 +0100

    test

---
+++ Projekt Sklep/Controllers/Adres/AdresTest.cs
@@ -0,0 +1,127 @@
+using Xunit;
+using Microsoft.AspNetCore.Mvc;
+using Projekt_Sklep.Controllers.Adres;
+using Projekt_Sklep.Models.Adres;
+using System;
+
+namespace Projekt_Sklep.Tests
+{
+    public class AdresControllerTests
+    {
+        private readonly AdresController _controller;
+
+        public AdresControllerTests()
+        {
+            _controller = new AdresController();
+        }
+
+        
+        [Fact]
+        public void GetAll_ReturnsOkResult()
+        {
+            // Act
+            var result = _controller.GetAll();
+
+            // Assert
+            Assert.IsType<ActionResult<IEnumerable<Models.Adres.Adres>>>(result);
+
+           
+                Assert.IsType<OkObjectResult>(result.Result);
+
+                var okResult = result.Result as OkObjectResult;
+                var adresList = okResult?.Value as List<Models.Adres.Adres>;
+
+                Assert.NotNull(adresList);
+                Assert.NotEmpty(adresList);
+
+                
+        }
+
+
+        [Fact]
+        public void GetById_ReturnsNotFoundResult_WhenIdDoesNotExist()
+        {
+            // Arrange
+            Guid id = Guid.NewGuid();
+
+            // Act
+            var result = _controller.GetById(id);
+
+            // Assert
+            Assert.IsType<ActionResult<Models.Adres.Adres>>(result);
+            Assert.IsType<NotFoundResult>(result.Result);
+        }
+
+        [Fact]
+        public void CreateAdresEntity_ReturnsCreatedAtActionResult()
+        {
+            // Arrange
+            var newAdres = new Models.Adres.Adres
+            {
+                // Set properties for the new Adres
+            };
+
+            // Act
+            var result = _controller.CreateAdresEntity(newAdres);
+
+            // Assert
+            Assert.IsType<ActionResult<Models.Adres.Adres>>(result);
+            Assert.IsType<ObjectResult>(result.Result);
+        }
+
+        [Fact]
+        public void CreateAdresEntity_ReturnsBadRequestResult_WhenInvalidData()
+        {
+            // Arrange
+            Models.Adres.Adres invalidAdres = null;
+
+            // Act
+            var result = _controller.CreateAdresEntity(invalidAdres);
+
+            // Assert
+            Assert.IsType<ActionResult<Models.Adres.Adres>>(result);
+            Assert.IsType<BadRequestObjectResult>(result.Result);
+        }
+
+        [Fact]
+        public void DeleteKlientEntity_ReturnsNoContentResult()
+        {
+            // Arrange
+            Guid id = Guid.NewGuid();
+
+            // Act
+            var result = _controller.DeleteKlientEntity(id);
+
+            // Assert
+            Assert.IsType<NotFoundResult>(result);
+            
+        }
+
+        [Fact]
+        public void DeleteKlientEntity_ReturnsNotFoundResult_WhenIdDoesNotExist()
+        {
+            // Arrange
+            Guid nonExistentId = Guid.NewGuid();
+
+            // Act
+            var result = _controller.DeleteKlientEntity(nonExistentId);
+
+            // Assert
+            Assert.IsType<NotFoundResult>(result);
+            
+        }
+
+        [Fact]
+        public void EditKlientEntity_ReturnsTrue_WhenEditSuccessful()
+        {
+            // Arrange
+            Guid id = Guid.NewGuid();
+
+            // Act
+            var result = _controller.EditKlientEntity(new Guid("c5c4793e-984b-46c4-b43c-804e2253c2f5"), "12345", "City", "Province", "Country");
+
+            // Assert
+            Assert.True(result);
+        }
+    }
+}
