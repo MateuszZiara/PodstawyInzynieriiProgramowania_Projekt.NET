fb297233e2918ee127cbe0328f2fde8ad22b5fda
Przemek <s092767@student.kielce.pl>
2024-01-24 22:08:13 +0100

    gb

---
+++ Projekt Sklep/Pages/Wykup_Polise.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Projekt_Sklep.Pages
+{
+    public class Wykup_PoliseModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
