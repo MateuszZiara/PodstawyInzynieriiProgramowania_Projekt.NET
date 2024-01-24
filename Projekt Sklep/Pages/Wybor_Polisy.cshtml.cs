8a4ac3d07772f6def2fe02a61093fad4323db298
Przemek <s092767@student.kielce.pl>
2024-01-25 00:32:19 +0100

    g

---
+++ Projekt Sklep/Pages/Wybor_Polisy.cshtml.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Projekt_Sklep.Pages
+{
+    public class Wybor_PolisyModel : PageModel
+    {
+        public void OnGet()
+        {
+
+            
+        }
+    }
+}
--- Projekt Sklep/Pages/Wykup_Polise.cshtml.cs
+++ Projekt Sklep/Pages/Wykup_Polise.cshtml.cs
@@ -8,5 +8,23 @@ namespace Projekt_Sklep.Pages
         public void OnGet()
         {
         }
+        public IActionResult OnPost(string policyType)
+        {
+            switch (policyType)
+            {
+                case "life":
+                    return RedirectToPage("/Wybor_Polisy", new { policyType = "life", additionalData = "dane_dla_zycia" });
+                case "car":
+                    return RedirectToPage("/Wybor_Polisy", new { policyType = "car", additionalData = "dane_dla_samochodu" });
+                case "property":
+                    return RedirectToPage("/Wybor_Polisy", new { policyType = "property", additionalData = "dane_dla_nieruchomosci" });
+                case "firm":
+                    return RedirectToPage("/Wybor_Polisy", new { policyType = "firm", additionalData = "dane_dla_firmy" });
+                case "group":
+                    return RedirectToPage("/Wybor_Polisy", new { policyType = "group", additionalData = "dane_dla_grupy" });
+                default:
+                    return RedirectToPage("/Wybor_Polisy");
+            }
+        }
     }
 }
